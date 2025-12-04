c92c0cd1de850b6b8044ed51dbf1b58f33b76a0b
PabloFC <pablofc_68@hotmail.com>
2025-12-04 19:21:34 +0100

    tailwind reinstalled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
-import "./App.css";
-
 function App() {
   return (
     <>
